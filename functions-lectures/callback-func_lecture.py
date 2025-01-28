1f64cfd48273bbba38cdfce82612a965a3ed0872
daniilbelik94 <161323222+daniilbelik94@users.noreply.github.com>
2025-01-28 17:44:46 +0100

    add functions to demonstrate callback usage with number processing and data handling

--- functions-lectures/callback-func_lecture.py
+++ functions-lectures/callback-func_lecture.py
@@ -28,10 +28,32 @@ def process_numbers(num, callback_fn):
 
  # основной части кода пользователь вводит число с помощью функции input, преобразует его в целое число и передает в
  # функцию process_numbers вместе с функцией обратного вызова print_number_info:
+def print_square_number(num):
+    print(f'{num} in square is {num**2}')
 
 entered_num = int(input('Enter a number: '))
 process_numbers(entered_num, print_number_info)
+process_numbers(entered_num, print_square_number)
 
 # Таким образом, когда пользователь вводит число, функция process_numbers вызывает функцию print_number_info,
 #  которая определяет, является ли введенное число четным или нечетным, и выводит соответствующее сообщение.
+
+
+def send_data(data):
+    pass
+
+def process_data(input_data, send_data_fn):
+    updated_data = input_data.copy()
+
+    send_data_fn(updated_data)
+
+process_data({'name': 'John', 'age': 25}, send_data)
+
+# В приведенном коде определены две функции: send_data и process_data.
+#  Функция send_data принимает один параметр data и выполняет какие-то действия с этими данными.
+#  Функция process_data принимает два параметра: input_data и функцию обратного вызова send_data_fn.
+#  Внутри функции process_data создается копия входных данных input_data, которая затем передается в функцию обратного вызова send_data_fn.
+
+
+
 
